8189b217bbc7c005eeade723560f87060d45f8f7
Blitzkriegtech <aljunrendon@yahoo.com>
2025-03-03 15:56:13 +0800

    finish all game logics

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,7 +2,7 @@
 
 # Class file that handles all the game flow of hangman
 class Hangman
-  attr_reader :board, :secret_word, :guessed_letters, :wrong_letters
+  attr_reader :board, :secret_word, :guessed_letters, :wrong_letters, :lives
 
   def initialize
     @secret_word = gen_secret_word
@@ -16,6 +16,12 @@ class Hangman
     puts "\nWelcome to HANGMAN! >_<"
     puts "\nGame commencing...\nSECRET WORD has #{secret_word.size} letters."
     display_board
+    until game_over?
+      guess = valid_guess
+      update_board(guess)
+      display_game_state
+    end
+    conclude_game
   end
 
   def parse_txt
@@ -39,11 +45,11 @@ class Hangman
   end
 
   def display_board
-    puts board.join(' ')
+    puts "\nSECRET WORD: #{board.join(' ')}"
   end
 
   def display_wrong_letters
-    wrong_letters.join('')
+    wrong_letters.join(' ')
   end
 
   def valid_guess
@@ -60,13 +66,41 @@ class Hangman
     end
   end
 
+  def update_board(guess)
+    if secret_word.include?(guess)
+      secret_word.each_char.with_index { |letter, i| board[i] = letter if letter == guess }
+    else
+      wrong_letters << guess
+      @lives -= 1
+    end
+    guessed_letters << guess
+  end
+
+  def display_game_state
+    display_board
+    puts "Incorrect guess(es): #{display_wrong_letters}" unless wrong_letters.empty?
+    puts "LIVES LEFT: #{lives}"
+  end
+
   def already_guessed?(letter)
     guessed_letters.include?(letter)
+  end
+
+  def game_over?
+    lives.zero? || !board.include?('_')
   end
 
   def quit_game
     puts "Thank you for playing!\nBTW the SECRET WORD was: #{secret_word}."
     exit
+  end
+
+  def conclude_game
+    if lives.zero?
+      puts "\nThank you for playing!\nSadly you lose :(\nSECRET WORD: #{secret_word}"
+    else
+      puts "YOU WON!\nYou have guessed the SECRET WORD: #{secret_word}"
+    end
   end
 end
 Hangman.new.play
