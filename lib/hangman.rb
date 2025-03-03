d6d1cc6a512b8fb47001fc3402248cb60d39d558
Blitzkriegtech <aljunrendon@yahoo.com>
2025-03-03 14:50:56 +0800

    add #parse_txt

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -11,4 +11,20 @@ class Hangman
     @wrong_letters = []
     @board = []
   end
+
+  def play
+    puts "\nWelcome to HANGMAN! >_<"
+    puts "Newgame commence...\nSECRET WORD has #{secret_word.size} letters."
+  end
+
+  def parse_txt
+    File.open('word.txt', 'r') do |file|
+      file.each_line.with_object([]) do |word, arr|
+        stripped_word = word.strip
+        arr << stripped_word if (5..12).include?(stripped_word.size) && stripped_word.match?(/\A[a-z]+\z/)
+      end
+    end
+  end
 end
+
+Hangman.new.parse_txt
